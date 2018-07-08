614ee6d4c9697f8a3d3112e6d3a9d19b3ea39b25
Vinicius Vasconcelos <www.vasconcelos@gmail.com>
2018-07-08 19:18:54 -0300

    Atualizado game.js com loading

--- js/game.js
+++ js/game.js
@@ -5,11 +5,16 @@ window.onload = function() {
 		parent: 'Template',
 		width: 500,
 		height: 600,
+		background: '#ffffff',
 		scene: {
 			preload: preload,
 			create: create
 		}
 	};
+
+	/*	Cria as variáveis que armazenarão a largura e altura da tela, 
+		e o centro X e Y dela, passadas no preload() */
+	var largura, altura, centroX, centroY;
 
 	//Cria um novo objeto Phaser Game
 	var game = new Phaser.Game(config);
@@ -19,9 +24,18 @@ window.onload = function() {
 		this.load.image('logo', 'assets/sprites/logo.png');
 		
 		//Looping apenas para demorar mais pra carregar e testar o código
-	    for (var i = 0; i < 100; i++) {
-	        this.load.image('logo'+i, 'assets/sprites/logo.png');
+	    for (var i = 0; i < 20; i++) {
+	        this.load.image('logo' + i, 'assets/sprites/logo.png');
 	    }
+
+	    //Largura e altura da tela
+	    largura = this.cameras.main.width;
+	    altura = this.cameras.main.height;
+	    console.log(altura);
+
+	    //Centros
+	    centroX = largura / 2;
+	    centroY = altura / 2;
 
 	    /*	Cria os GameObject.Graphics que serão usados na barra de loading
 	    	que será atualizado dentro do 'progress'. A caixaLoading é o
@@ -31,8 +45,46 @@ window.onload = function() {
 	    var barraLoading = this.add.graphics();
 	    var caixaLoading = this.add.graphics();
 	    caixaLoading.fillStyle(0x222222, 0.8);
-	    caixaLoading.fillRect(50, 300, 250, 25);
+	    caixaLoading.fillRect(centroX / 2, centroY, centroX, 25);
 	   // console.log("center " + game.getBounds().width);
+	    
+	    //Texto do loading
+	    var textoLoading = this.make.text({
+	    	x: centroX,
+	    	y: centroY - 50,
+	    	text: 'Carregando...',
+	    	style: {
+	    		font: '15px Monospace',
+	    		fill: '#ffffff'
+	    	}
+	    });
+	    textoLoading.setOrigin(0.5, 0.5);
+
+	    //Texto progresso
+	    var textoProgresso = this.make.text({
+	   		x: centroX,
+	   		y: centroY + 14,
+	   		text: '0%',
+	   		style: {
+	   			font: '14px Monospace',
+	   			fill: '#ffffff'
+	   		}
+	   });
+	   textoProgresso.setOrigin(0.5, 0.5);
+
+	   //Texto do arquivo carregado
+	    var textoArquivo = this.make.text({
+	    	x: centroX,
+	    	y: centroY + 50,
+	    	text: '',
+	    	style: {
+	    		font: '15px Monospace',
+	    		fill: '#ffffff'
+	    	}
+	    });
+	    textoArquivo.setOrigin(0.5, 0.5);
+
+
 
 	    /*	Listener que checa pelo evento 'progress' que é emitido pelo
 			LoaderPlugin do Phaser. Ele ocorre a cada vez que um arquivo é
@@ -40,13 +92,15 @@ window.onload = function() {
 			um valor entre 0 e 1, que pode ser usado para acompanhar o
 			progresso geral do processo de loading */
 	    this.load.on('progress', function(value) {
-	    	console.log("progress " + value);
 
 	    	/*	A barraLoading crescerá aumentando a largura (width)
 	    		conforme o 'value' do 'progress' é recebido. */
 	    	barraLoading.clear();
 	    	barraLoading.fillStyle(0xffffff, 1);
-	    	barraLoading.fillRect(55, 305, 230 * value, 17);
+	    	barraLoading.fillRect(centroX / 2 + 5, centroY + 4, (centroX - 15) * value, 17);
+
+	    	//O value é multiplicado por 100 porque o valor dele é entre 0 e 1
+	    	textoProgresso.setText(parseInt(value * 100) + '%');
 	    });
 	    /*	Listener que checa pelo evento 'progress' que é emitido pelo
 			LoaderPlugin do Phaser. Ele ocorre a cada vez que um arquivo é
@@ -54,7 +108,9 @@ window.onload = function() {
 			um objeto contendo informações sobre o arquivo que recém foi
 			carregado. */
 	    this.load.on('fileprogress', function(file) {
-	    	console.log("fileprogress " + file.src);
+	    	//Atualiza o texto com o arquivo sendo carregado no momento
+	    	textoArquivo.setText('Carregando arquivo: ' + file.key);
+	    	//console.log("fileprogress " + file.src);
 	    });
 	     /*	Listener que checa pelo evento 'complete' que é emitido pelo
 			LoaderPlugin do Phaser. Ele ocorre apenas uma vez, somente quando
@@ -62,14 +118,18 @@ window.onload = function() {
 	    this.load.on('complete', function() {
 	    	console.log('Completou');
 
-	    	//	Ao completar o loading, destrói ambos os objetos
+	    	//	Ao completar o loading, destrói os objetos
 	    	barraLoading.destroy();
 	    	caixaLoading.destroy();
+	    	textoLoading.destroy();
+	    	textoProgresso.destroy();
+	    	textoArquivo.destroy();
 	    });
 	}
 	function create() {
-		//Adiciona o logo na tela
-		var logo = this.add.image(200, 200, 'logo');
+		//Adiciona o logo centralizado
+		var logo = this.add.image(centroX, centroY, 'logo');
+		logo.setOrigin(0.5, 0.5);
 		console.log("FOI");
 
 	}
